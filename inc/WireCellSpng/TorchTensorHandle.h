6f40638e715adc21de27971d22a7b6e809d0e753
Jacob Calcutt <calcuttj@asl164.bnl.gov>
2024-12-04 10:34:18 -0500

    Fixing indentation. Adding in 'requires_grad(false)' when making handle from data

--- inc/WireCellSpng/TorchTensorHandle.h
+++ inc/WireCellSpng/TorchTensorHandle.h
@@ -15,29 +15,31 @@ namespace WireCell {
                       at::IntArrayRef sizes,
                       torch::Device device=torch::kCPU){
 
-  if (device == torch::kCUDA) {
-    //If we're using cuda, allocate memory on the device (TODO -- check which device)
-    //and copy data into the pointer (on device)
-    cudaMalloc((void **)&m_data, input.size() * sizeof(T));
-    cudaMemcpy(m_data, &input[0], input.size() * sizeof(T),
-               cudaMemcpyHostToDevice);
-  }
-  else {
-    //Do the same thing on host (CPU)
-    m_data = malloc(input.size()*sizeof(T));
-    memcpy(m_data, &input[0], input.size()*sizeof(T));
-  }
+      if (device == torch::kCUDA) {
+        //If we're using cuda, allocate memory on the device (TODO -- check which device)
+        //and copy data into the pointer (on device)
+        cudaMalloc((void **)&m_data, input.size() * sizeof(T));
+        cudaMemcpy(m_data, &input[0], input.size() * sizeof(T),
+                  cudaMemcpyHostToDevice);
+      }
+      else {
+        //Do the same thing on host (CPU)
+        m_data = malloc(input.size()*sizeof(T));
+        memcpy(m_data, &input[0], input.size()*sizeof(T));
+      }
 
-  auto options = torch::TensorOptions().dtype(get_dtype(input))
-                                       .device(device);
+      //Get the type of data, turn off comp. graph, and send to device.
+      auto options = torch::TensorOptions().dtype(get_dtype(input))
+                                           .requires_grad(false)
+                                           .device(device);
 
-  //Create the tensor from the data we imported
-  m_tensor = torch::from_blob(
-      m_data,
-      sizes,
-      options
-  );
-}
+      //Create the tensor from the data we imported
+      m_tensor = torch::from_blob(
+          m_data,
+          sizes,
+          options
+      );
+    }
     
     TorchTensorHandle(torch::Tensor & input_tensor);
     ~TorchTensorHandle();
