40cf7d9e6156e246537aa8ef6b810dcc8e00846a
physnerds <amitbashyal@gmail.com>
2025-05-29 12:12:14 -0400

    Initial commits for the DNN ROI related works

---
+++ inc/WireCellSpng/ITorchSet.h
@@ -0,0 +1,25 @@
+#ifndef WIRECELL_ITorchSET
+#define WIRECELL_ITorchSET
+
+#include "WireCellIface/ITorch.h"
+#include "WireCellUtil/Configuration.h"
+
+
+namespace WireCell {
+
+    class ITorchSet : public IData<ITorchSet> {
+       public:
+        virtual ~ITorchSet() {}
+
+        /// Return some identifier number that is unique to this set.
+        virtual int ident() const = 0;
+
+        /// Optional metadata associated with the set of Torchs
+        virtual Configuration metadata() const { return Configuration(); }
+
+        /// Return the Torchs in this set.
+        virtual ITorch::shared_vector Torchs() const = 0;
+    };
+}  // namespace WireCell
+
+#endif
---
+++ inc/WireCellSpng/TorchService.h
@@ -0,0 +1,44 @@
+/** Apply a torch script module to "forward" an input tensor. */
+
+#ifndef WIRECELLPYTORCH_TORCHSERVICE
+#define WIRECELLPYTORCH_TORCHSERVICE
+
+#include "WireCellIface/IConfigurable.h"
+#include "WireCellIface/ITensorForward.h"
+#include "WireCellUtil/Logging.h"
+#include "WireCellAux/Logger.h"
+#include "WireCellPytorch/TorchContext.h"
+
+#include "WireCellPytorch/Torch.h"  // One-stop header.
+
+namespace WireCell::Pytorch {
+    class TorchService : public Aux::Logger,
+                         public ITensorForward,
+                         public IConfigurable
+    {
+      public:
+        TorchService();
+        virtual ~TorchService() {}
+
+        // IConfigurable interface
+        virtual void configure(const WireCell::Configuration& config);
+        virtual WireCell::Configuration default_configuration() const;
+
+        // ITensorSetFilter interface.  This is thread-safe.
+        virtual ITensorSet::pointer forward(const ITensorSet::pointer& input) const;
+
+      private:
+
+        // for read-only access, claim is that .forward() is thread
+        // safe.  However .forward() is not const so we must make this
+        // mutable.
+        mutable torch::jit::script::Module m_module;
+
+        // Even though thread safe, we want to honor a per device
+        // semaphore to give user chance ot limit us.
+        TorchContext m_ctx;
+
+    };
+}  // namespace WireCell::Pytorch
+
+#endif  // WIRECELLPYTORCH_TORCHSERVICE
